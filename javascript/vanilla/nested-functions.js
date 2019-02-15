c8d38ed8ab00b1ff3a22f8bf1817764b58643538
Martin Tomsik <martin@internetmate.uk>
2019-02-15 19:13:15 +0000

    Improved JS: Nested Functions example.

--- javascript/vanilla/nested-functions.js
+++ javascript/vanilla/nested-functions.js
@@ -2,21 +2,20 @@
 
 function ageInYear(yearInFuture) {
 	return function(yearOfBirth) {
-		let ageInYear = yearInFuture - yearOfBirth;
-		return ageInYear;
+		return yearInFuture - yearOfBirth;
 	}
 }
 
 // Call option #1.
 let yearInFutureJohn = ageInYear(2078);
-console.log("Jonh's age in year 2078: ", yearInFutureJohn(1978));
+console.log("Jonh's age in year 2078 (#1): ", yearInFutureJohn(1978));
 
 // Call option #2.
-console.log("Jonh's age in year 2078: ", ageInYear(2078)(1978));
+console.log("Jonh's age in year 2078 (#2): ", ageInYear(2078)(1978));
 
 // Call option #1.
 let yearInFutureJane = ageInYear(2000);
-console.log("Jane's age in year 2000: ", yearInFutureJane(1980));
+console.log("Jane's age in year 2000 (#1): ", yearInFutureJane(1980));
 
 // Call option #2.
-console.log("Jane's age in year 2000: ", ageInYear(2000)(1980));
+console.log("Jane's age in year 2000 (#2): ", ageInYear(2000)(1980));
